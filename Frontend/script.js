81ba2e07e81c4d467d3c2638ffb0fbbec238f978
Angel Morales <angelmoralesb@gmail.com>
2021-11-17 21:51:43 -0500

    estilos css

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,6 @@
-let elemento = document.getElementsByTagName("h1");
+/*let elemento = document.getElementsByTagName("h1");
 for (let elem of elemento) {
     elem.style.color = "red";
     elem.style.backgroundColor = "yellow";
 }
+*/
