b52564c987ff150e9c5816859a22245ebe8215d5
Angel Morales <angelmoralesb@gmail.com>
2021-11-19 22:03:34 -0500

    javascript 2

--- Frontend/script.js
+++ Frontend/script.js
@@ -98,6 +98,129 @@ function tecla() {
     console.log("Tecla");
 }
 
+//javascript 2
+
+let persona = new Object();
+
+persona.nombre = "Juan";
+persona.apellido = "Perez";
+persona.edad = 30;
+
+console.log(persona);
+
+let persona2 = {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 30,
+    detalle : function() {
+        console.log(this.nombre + " " + this.apellido + " " + this.edad);
+    }
+};
+
+console.log(persona2.nombre);
+console.log(persona2["nombre"]);
+persona2.direccion = "Calle falsa 123";
+console.log(persona2);
+persona2.detalle();
+//persona2.detalle2 = () => {
+//    console.log(persona2.nombre + " " + persona2.apellido + " " + persona2.edad);
+//}
+
+let numeros = [1, persona2, "saludo", 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numeros.length);
+
+console.log(numeros.push(100));
+console.log(numeros);
+numeros.pop();
+console.log(numeros);
+numeros.unshift(500);
+console.log(numeros);
+numeros.shift();
+console.log(numeros);
+
+let numeros2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let concatenado = numeros.concat(numeros2);
+console.log(concatenado);
+
+let slice =  numeros.slice(4, 6);
+console.log(slice);
+
+//let splice2 = numeros.splice(3, 2);
+//console.log(splice2);
+console.log(numeros2.join(" - "));
+console.log(numeros.includes(5));
+
+//numeros.fill(0, 4, 9);
+console.log(numeros);
+numeros.reverse();
+console.log(numeros);
+
+let palabras = ["Hola", "mundo" , "como", "estas"];
+console.log(palabras.sort());
+
+ let ordenado = numeros.sort((a, b) => {
+    return a - b;
+});
+
+console.log(ordenado);
+
+//for (const numero of ordenado) {
+//    console.log(numero);
+//}
+
+//ordenado.forEach(numero => {
+//    console.log(numero);
+//});
+
+let IsPositivo = numeros2.every(numero => {
+    return numero > 0;
+});
+
+console.log(IsPositivo);
+
+let IsHere = numeros.some(numero => {
+    return numero == 5;
+});
+
+console.log(IsHere);
+
+let nuevoArray = palabras.map(palabra => {
+    return palabra + " nuevo";
+});
+console.log(nuevoArray);
+
+let nuevoArraynumeros = numeros.map(numero => {
+    return numero * 2;
+});
+
+console.log(nuevoArraynumeros);
+
+let id = 5;
+
+let filter = numeros.filter(numero => {
+    return numero != id;
+});
+
+console.log(filter);
+
+let nombre2 =  "Juan";
+let persona3 = numeros.find(persona => {
+    return persona.nombre == nombre2;
+});
+
+console.log(persona3.apellido);
+
+let sumatoria = numeros2.reduce((valorAnterior, valorActual) => {
+    console.log("Datos "  + valorAnterior + " " +valorActual);
+    return valorAnterior + valorActual;
+});
+
+console.log(sumatoria);
+
+
+
+
 
 
 
