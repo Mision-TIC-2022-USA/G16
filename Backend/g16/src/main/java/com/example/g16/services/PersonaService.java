ba3df83a81a0509dfc6ba05a840bd452a7b7d266
Angel Morales <angelmoralesb@gmail.com>
2021-11-24 21:45:34 -0500

    mongo sring

---
+++ Backend/g16/src/main/java/com/example/g16/services/PersonaService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.g16.services;
+
+import com.example.g16.Models.Persona;
+import com.example.g16.repositories.PersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ASUS RYZEN
+ */
+@Service
+public class PersonaService {
+
+    @Autowired
+    private PersonaRepository personaRepository;
+
+    public List<Persona> getAll() {
+        return personaRepository.getAll();
+    }
+}
---
+++ Backend/g16/src/test/java/com/example/g16/G16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
