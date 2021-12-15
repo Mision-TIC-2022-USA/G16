9ac3499a971aafc2a4d4f2aff4544831d7fe8ea6
Angel Morales <angelmoralesb@gmail.com>
2021-12-15 15:10:19 -0500

    Axios react

--- Frontend/react/pruebareact/src/componentes/HelloMessage.js
+++ Frontend/react/pruebareact/src/componentes/HelloMessage.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Datacontext } from "./DataContext";
 import "./HelloMessage.css"
+import { getPersonas as getPersonasApi } from "../servicios/PersonasService";
+import { Spinner } from "react-bootstrap";
 class HelloMessage extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            personas: [],
+            loading: true
+        };
+    }
     static contextType = Datacontext;
     //Montaje
     componentDidMount() {
         console.log("HelloMessage.componentDidMount");
+        getPersonasApi().then(personas => {
+            this.setState(() => ({
+                personas: personas.data,
+                loading: false
+            }));
+            console.log("personas", personas.data);
+        });
     }
 
     //Actualizacion
@@ -31,12 +46,43 @@ class HelloMessage extends React.Component {
             return <h2>Hola desconocido</h2>
         }
 
-        const {user} = this.context;
+        const { user } = this.context;
         console.log(user);
 
         return (
             <>
                 <div className="titulo" >{getSaludo(user)} </div>
+                {/* Table beautiful loading personas */}
+
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th>Edad</th>
+                            <th>Correo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.loading ?
+                                (
+                                    <tr>
+                                        <td colSpan="4" className="text-center">
+                                            <Spinner animation="grow" />
+                                        </td>
+                                    </tr>
+                                ) :
+                                (this.state.personas.map(persona => (
+                                    <tr key={persona.id}>
+                                        <td>{persona.id}</td>
+                                        <td>{persona.nombre}</td>
+                                        <td>{persona.edad}</td>
+                                        <td>{persona.correo}</td>
+                                    </tr>
+                                )))}
+                    </tbody>
+                </table>
             </>
         )
     }
